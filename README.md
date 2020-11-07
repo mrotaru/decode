21a543a8330a475d06398245b1674d97c82c00b9
Mihai Rotaru <mihai.rotaru@gmx.com>
2020-11-07 11:12:26 +0000

    bug fixes, update docs

--- README.md
+++ README.md
@@ -1,7 +1,65 @@
 # decode
 
-WIP
+Try to match input against URL, base64 and JWT encodings, and attempt decoding it.
 
-Small utility which takes a string as input and processes it by using simple heuristics to determine encoding, and decode it.
+To install: `npm i -g mrotaru/decode`
 
-By default, it will check if input is encoded as URL, base64 or JWT - in that order. At each step, if encoding is matched and decoding is successful, the result will be matched against the following encoding, and a decoding attempt will be made - with the result passed on. This continues until there are no more encodings to try. If a decoding attempt fails, input is passed on as-is.
+Output of `--help` below.
+
+```
+Functional Options
+
+  All of these are boolean and can be negated by prefixing with "no-"; for
+  example, to disable piping, use the '--no-pipe' option. Note that some are
+  disabled by default - --bail and --first.
+
+    --match        attempt to match input to an encoding before decoding (default: true)
+    --decode       attempt to decode input (default: true)
+    --pipe         pass the result of each decoding as the input for the following match/decode (default: true)
+    --bail         exit on first decode failure, or first match failure if --no-decode (default: false)
+    --first        exit on first successful decode, or first successful match if --no-decode (default: false)
+
+Other
+
+    --verbose      verbose output - more details on how input is matched against encodings and decoded
+    --version      show version number
+    --help         show this message
+    --dump-config  dump configuration state
+
+Examples
+
+  $ decode Zm9vIGJhcg==
+  foo bar
+
+  $ decode https%3A%2F%2Flocalhost%3A8080%2Ffoo%2Fbar
+  https://localhost:8080/foo/bar
+
+  $ decode --verbose Zm9vIGJhcg==
+  [ url ] matching "Zm9vIGJhcg=="
+  [ url ] did not match
+  [ base64 ] matching "Zm9vIGJhcg=="
+  [ base64 ] matched
+  [ base64 ] decoding "Zm9vIGJhcg=="
+  [ base64 ] decoded: "foo bar"
+  [ base64 ] passing decoded value to next match/encode (use --no-pipe to avoid this)
+  [ jwt ] matching "foo bar"
+  [ jwt ] did not match
+  foo bar
+
+  $ decode --no-decode Zm9vIGJhcg==
+  base64
+
+  $ decode eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+  {
+    "header": {
+      "alg": "HS256",
+      "typ": "JWT"
+    },
+    "payload": {
+      "sub": "1234567890",
+      "name": "John Doe",
+      "iat": 1516239022
+    },
+    "signature": "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  }
+```
--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 const { output, levels } = require("./output");
-const { encodings, decode } = require("./lib")
+const { encodings } = require("./lib");
+const fs = require('fs');
 const version = require("./package.json").version;
 
 const encodingsOrder = ["url", "base64", "jwt"];
@@ -12,19 +13,20 @@ const config = {
   // name: [ current value, current value source (def/cli/user), default value, description ]
   match:   [true,  true,  "default", "attempt to match input to an encoding before decoding"],
   decode:  [true,  true,  "default", "attempt to decode input"],
-  pipe:    [true,  true,  "default", "pass the output of an encoding as the input for the following match/decode"],
+  pipe:    [true,  true,  "default", "pass the result of each decoding as the input for the following match/decode"],
   bail:    [false, false, "default", "exit on first decode failure, or first match failure if --no-decode"],
   first:   [false, false, "default", "exit on first successful decode, or first successful match if --no-decode"],
 };
 
 const options = [
-  ["verbose", "verbose output", () => { config.verbosity = levels.VERBOSE; } ],
+  ["verbose", "verbose output - more details on how input is matched against encodings and decoded", () => { config.verbosity = levels.VERBOSE; } ],
   ["version", "show version number", () => { console.log(`decode v.${version}`); process.exit(0); } ],
   ["help", "show this message", () => { printHelp(); process.exit(0); } ],
-  ["dump-config", "show configuration state before input", () => { config.dumpConfig = true } ],
+  ["dump-config", "dump configuration state", () => { config.dumpConfig = true } ],
 ];
 
 const freeArgs = [];
+let needInput = false;
 const args = process.argv.slice(2);
 while (args.length) {
   const arg = args.shift();
@@ -35,8 +37,10 @@ while (args.length) {
       opt[2]()
     } else {
       if (config.hasOwnProperty(stripped)) {
+        needInput = true;
         ensureBooleanAndSet(stripped, true, arg);
       } else if (stripped.match(/^no-[a-zA-Z]/)) {
+        needInput = true;
         ensureBooleanAndSet(stripped.replace(/^no-/, ""), false, arg);
       } else {
         fatal(`Unrecognized argument: ${arg}`, stripped);
@@ -47,7 +51,7 @@ while (args.length) {
   }
 }
 
-if (freeArgs.length !== 1) {
+if (freeArgs.length !== 1 && needInput) {
   fatal('Provide one argument as the input to be matched/decoded; use --help for more information.')
 }
 
@@ -63,66 +67,93 @@ function ensureBooleanAndSet(optionName, value, arg) {
 }
 
 function printHelp() {
-  console.log(`\ndecode v.${version}\n`)
+  console.log(`\ndecode v.${version}\n`);
+  console.log(`Try to match input against URL, base64 and JWT encodings, and attempt decoding it.\n`);
+  console.log(`Functional Options`);
+  console.log(`
+  All of these are boolean and can be negated by prefixing with "no-"; for
+  example, to disable piping, use the '--no-pipe' option. Note that some are
+  disabled by default - --bail and --first.
+  `);
   Object.keys(config).forEach(name => {
     if (Array.isArray(config[name])) {
       const [val, defaultValue, _, desc] = config[name];
-      console.log(`    --${name.padEnd(15)}`, desc, `(default: ${defaultValue})`);
+      console.log(`    --${name.padEnd(12)}`, desc, `(default: ${defaultValue})`);
     }
   })
-  console.log(`
-    --help - show this message
-    --version - show version number
-    --verbose - verbose output
-  `);
+  console.log(`\nOther\n`);
+  options.forEach(option => {
+    const [name, desc] = option;
+    console.log(`    --${name.padEnd(12)}`, desc);
+  });
+  console.log(`\nExamples\n`);
+  fs.readFileSync("./EXAMPLES.txt", { encoding: 'utf8' }).split('\n').forEach(line => {
+    console.log(`  ${line}`);
+  })
+
+  console.log();
 }
 
 const out = output(config.verbosity);
 
-if (config.dumpConfig) {
-  console.log(JSON.stringify(config, null, 4))
-}
 const cfg = Object.keys(config).reduce((acc, curr) => {
   acc[curr] = Array.isArray(config[curr]) ? config[curr][0] : config[curr];
   return acc;
 }, {})
 
+if (config.dumpConfig) {
+  console.dir(cfg);
+}
+
+let latestSuccess = null;
+
 const matchDecode = (input) => {
   let result = input;
-  for (const encodingName of encodingsOrder) {
-    out.scope(encodingName);
-    let matched = false;
+  for (let i = 0; i < encodingsOrder.length; i++) {
+    const encodingName = encodingsOrder[i];
+    const isLast = i === encodingsOrder.length -1;
     const encoding = encodings[encodingName];
+    out.scope(encodingName);
+
+    let matched = false;
     if (cfg.match) {
-      out.debug(`matching "${result}"...`);
+      out.debug(`matching "${result}"`);
       if (encoding.match(result)) {
         matched = true;
-        out.debug(encoding.match(result) ? 'matched' : 'did not match');
-        if (!cfg.decode && cfg.first) {
-          out.finalResult(`${result} matched "${encodingName}" encoding; exiting due to --first`)
+        out.debug('matched');
+        if (!cfg.decode) {
+          if (cfg.first) {
+            out.debug(`${result} matched "${encodingName}" encoding; exiting due to --first`)
+            out.finalResult(encodingName)
+          } else if (!cfg.decode) {
+            latestSuccess = encodingName;
+          }
         }
       } else {
-        out.debug(`input "${result}" does not seem to be encoded with "${encodingName}"`);
+        out.debug('did not match');
       }
     }
+
     if (cfg.decode) {
       if (cfg.match && !matched) {
         continue;
       }
-      out.debug(`decoding "${result}"...`);
+      out.debug(`decoding "${result}"`);
       try {
         const decoded = encoding.decode(result);
+        latestSuccess = decoded;
         out.debug(`decoded: "${decoded}"`);
         if (cfg.first) {
-          out.debug("printing decoded value, then exiting due to --first...");
+          out.debug("printing decoded value, then exiting due to --first");
           out.finalResult(decoded)
         }
+        isLast && finalResult(decoded);
         if (cfg.pipe) {
-          out.debug(`passing decoded value to next match/encode...`);
-          result = decoded
+          !isLast && out.debug(`passing decoded value to next match/encode (use --no-pipe to avoid this)`);
+          result = decoded;
         }
       } catch (ex) {
-        out.debug(`could not decode "${result}"...`);
+        out.debug(`could not decode "${result}"`);
         if (cfg.bail) {
           out.error('decoding failed; exiting due to --bail')
           out.debug(ex)
@@ -134,7 +165,12 @@ const matchDecode = (input) => {
   return result;
 };
 
-matchDecode(freeArgs[0])
+if (freeArgs.length === 1) {
+  matchDecode(freeArgs[0])
+  if (latestSuccess) {
+    console.log(latestSuccess);
+  }
+}
 
 function fatal(msg) {
   console.error(msg);
